e6d1abd07d0a596c76a464addaa2e1282354a64d
Teo-Reat <teo.reat@gmail.com>
2020-05-07 16:05:15 +0300

    Project init

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h1>Dungeon Explore</h1>
+    <p>After cutting the garlics, flavor strawberries, seaweed and salsa verde with it in an ice blender.
+    All children like sliced raspberries in peanut butter juice and dill.
+    Place the strawberries in a casserole, and garnish smoothly with salted tabasco.
+    Juicy bok choy can be made sichuan-style by rinsing with joghurt.</p>
+    <p>All jolly rogers raid swashbuckling, big mates.
+    Amnesty ho! break to be hailled.
+    Waves are the ales of the salty faith.
+    Damn yer ale, feed the scallywag.
+    Ahoy, golden scallywag. you won't fight the captain's quarters.
+    Endurance, halitosis, and yellow fever.</p>
+    <router-link to="/game">Game</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
